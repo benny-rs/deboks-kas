b46301204eed263243e835c8862abec454ef8672
Nafis Handoko <naffisioner@gmail.com>
2022-04-16 15:32:42 +0700

    give datepicker default value when load

--- public/js/pencatatan2.js
+++ public/js/pencatatan2.js
@@ -1,9 +1,11 @@
 var dp = $('.datepicker').datepicker({
+    setDates: new Date(),
     autoclose: true,
     format: "MM - yyyy",
     startView: "months", 
     minViewMode: "months"
 });
+$('.datepicker').datepicker('setDate', new Date(window.location.pathname.split('/')[3], window.location.pathname.split('/')[4]-1, 1));
 dp.on('changeMonth', function(e){
     console.log(e.date);
     // $('#date-result').html(e.date.getMonth()+" "+e.date.getFullYear())
